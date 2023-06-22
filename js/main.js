7529ee015149f24636ac1bf5cdb288d21acc29cb
EvaAkim <evaakim2304@gmail.com>
2023-06-22 18:18:34 +0300

    map

--- js/main.js
+++ js/main.js
@@ -17,5 +17,15 @@ const swiper = new Swiper(".swiper", {
 
   ymaps.ready(init);
 
+  function init() {
+    let map = new ymaps.Map1('map1', {
+        center: [55.75554289958026,37.64131907421875],
+        zoom: 9
+
+    });
+  }
+
+  ymaps.ready(init);
+
 
   
